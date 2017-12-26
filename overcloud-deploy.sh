644c5d260f0ae316a7cf696f2c7844e956293d0f
Peng Liu <pliu@redhat.com>
2017-12-26 21:58:56 +0800

    ovs-dpdk-bond-vxlan

--- overcloud-deploy.sh
+++ overcloud-deploy.sh
@@ -4,7 +4,8 @@ exec openstack overcloud deploy \
         --templates /usr/share/openstack-tripleo-heat-templates \
         --ntp-server 192.168.23.1 \
         --control-flavor control --control-scale 1 \
-        --compute-flavor compute --compute-scale 2 \
+        --compute-flavor compute --compute-scale 1 \
         -e /root/templates-bond/network-isolation-absolute.yaml \
         -e /root/templates-bond/network-environment.yaml \
+        -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
         -e /root/templates-bond/environment.yaml
