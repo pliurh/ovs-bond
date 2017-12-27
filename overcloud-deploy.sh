9425d3132984c43fe56965729845afc6fef278bd
Peng Liu <pliu@redhat.com>
2017-12-27 12:18:40 +0000

    add ntp

--- overcloud-deploy.sh
+++ overcloud-deploy.sh
@@ -3,8 +3,8 @@
 exec openstack overcloud deploy \
         --templates /usr/share/openstack-tripleo-heat-templates \
         --ntp-server 192.168.23.1 \
-        --control-flavor control --control-scale 1 \
-        --compute-flavor compute --compute-scale 1 \
+        --control-flavor control --control-scale 3 \
+        --compute-flavor compute --compute-scale 2 \
         -e /root/templates-bond/network-isolation-absolute.yaml \
         -e /root/templates-bond/network-environment.yaml \
         -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
